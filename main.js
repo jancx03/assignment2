8fee8188346fa998b8f1070ff4df1109a1528120
Jhon Nieves <jancx031@gmail.com>
2021-10-07 14:14:45 -0400

    completed map

--- main.js
+++ main.js
@@ -7,3 +7,15 @@ for(let i = 0; i < this.length;i++)
     //element,index,array
     callbackFn(this[i],i,this);
 };
+
+// MAP //
+Array.prototype.myMap = function(callbackFn) {
+    data = [];
+    
+    for(let i = 0; i < this.length;i++)
+        data.push(callbackFn(this[i],i,this));
+    
+    return data;
+
+
+};
